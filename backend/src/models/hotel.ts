884a2762f3dc07848f8a56df3d3cf9fefa515f68
Navneet <navneet11vinod11@gmail.com>
2024-03-06 14:33:23 +0530

    hotel change

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -17,7 +17,7 @@ export type HotelType = {
     lastUpdated: Date;
 };
 
-const hotelSchema = new mongoose.Schema<HotelType>({
+const hotelSchema = new mongoose.Schema({
     userId: { type: String, required: true},
     name: {type: String, required: true},
     city: {type: String, required: true},
@@ -30,7 +30,7 @@ const hotelSchema = new mongoose.Schema<HotelType>({
     pricePerNight: {type: Number, required: true},
     starRating: {type: Number, required: true, min: 1, max: 5},
     imageUrls: [{type: String, required: true}],
-    lastUpdated: {type: Date, required: true}
+    lastUpdated: {type: Date, required: true},
 
 });
 
